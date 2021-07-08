3262129286c01f1848d360b77f825acb199483f9
Melanie Roussel <memelrou@gmail.com>
2021-07-08 19:51:13 +0200

    create server

---
+++ server/README.md
@@ -0,0 +1,13 @@
+# PokeNews Server
+
+## Launch server 
+You need to install yarn or npm.
+
+Go to server directory :`cd server`
+
+install dependencies with `npm install` or `yarn`
+
+Then launch with  `yarn start` or `npm start`
+
+There you go ! 
+
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const users = [{name: "Mela", password: "PokeNews"}, {name:"Dernier Cri", password:"SpecialUser"}];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/login', (req, res) => {
+    const user = users.find(user => (user.name === req.body.name) && (user.password === req.body.password));
+    if (!user) {
+        res.status(401).send('Wrong name or password')
+    }
+    else if (user.name) {
+        res.status(200).send('Logged in')
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port : ${port}`)
+})
